178b171fd0ca8e937b8a8ce98a9fda3542b125e2
takunology <m15025@g.metro-cit.ac.jp>
2019-11-26 02:40:07 +0900

    Version1.1.0
    MVVMパターンとMaterialDesignを用いて見やすいUIにしました。

---
+++ EMBATC/EMBATC/Views/ImportPicture.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMBATC
+{
+    /// <summary>
+    /// ImportPicture.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ImportPicture : UserControl
+    {
+        string Filepath;
+        public ImportPicture()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog(); //ファイルオープンのインスタンス化
+
+            dialog.Filter = "pngファイル (*.png)|*.png|jpegファイル (*.jpg)|*.jpg|bmpファイル (*.bmp)|*.bmp";
+
+            if (dialog.ShowDialog() == true)
+            {
+                Filepath = (dialog.FileName);
+            }
+            else
+            {
+                return;
+            }
+
+            content_path.Content = Filepath;
+            picture.Source = new BitmapImage(new Uri(Filepath)); //UIに画像を表示
+        }
+    }
+}
---
+++ EMBATC/EMBATC/Views/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMBATC
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImportPicture ip = new ImportPicture();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(ip);
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    //GridPrincipal.Children.Add(new UserControlEscolha());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TransitioningContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, (100 + (60*index)), 0, 0);
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
